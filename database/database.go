6748acc9d737bc59ffafc877729fff6919dc5ccd
Parv Javheri <parv.javheri@logicmonitor.com>
2024-01-24 00:30:08 +0530

    Added database package and it's implementation

---
+++ database/database.go
@@ -0,0 +1,43 @@
+package database
+
+type database interface {
+	Get() []Student
+	GetById(int) Student
+	Set(Student) Class
+	IsPass(Student) bool
+}
+
+type Class struct {
+	ClassName string
+	Students  []Student
+}
+
+type Student struct {
+	Name  string
+	Id    int
+	Grade int
+}
+
+func (c Class) Get() []Student {
+	return c.Students
+}
+func (c Class) GetById(id int) Student {
+	for _, i := range c.Students {
+		if i.Id == id {
+			return i
+		}
+	}
+	return Student{}
+}
+
+func (c Class) Set(s Student) Class {
+	c.Students = append(c.Students, s)
+	return c
+}
+
+func (c Class) IsPass(s Student) bool {
+	if s.Grade > 40 {
+		return true
+	}
+	return false
+}
--- main.go
+++ main.go
@@ -1,1 +1,33 @@
+package main
 
+import (
+	"example.com/m/database"
+	"fmt"
+)
+
+func main() {
+	var c database.Class
+	c = c.Set(database.Student{
+		Name:  "Parv",
+		Id:    1,
+		Grade: 55,
+	})
+	c = c.Set(database.Student{
+		Name:  "Atharva",
+		Id:    2,
+		Grade: 75,
+	})
+	c = c.Set(database.Student{
+		Name:  "Varad",
+		Id:    3,
+		Grade: 85,
+	})
+
+	fmt.Printf("Students : \n %+v \n", c.Get())
+	fmt.Printf("Student at id - %d : \n %+v \n", 1, c.GetById(1))
+	if c.IsPass(c.GetById(1)) {
+		fmt.Printf("Student at id - %d is pass", 1)
+	} else {
+		fmt.Printf("Student at id - %d is fail", 1)
+	}
+}
