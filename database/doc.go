9abaf963d3d6f39481cc7523a829510b02b081eb
Parv Javheri <parv.javheri@logicmonitor.com>
2024-01-24 12:52:58 +0530

    corrected vendor files

--- database/doc.go
+++ database/doc.go
@@ -2,7 +2,6 @@ package database
 
 import (
 	_ "github.com/golang/mock/mockgen/model"
-	_ "go.uber.org/mock/mockgen/model"
 )
 
 //go:generate mockgen -destination=./mocks/db_mock.go -package=database . Db
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module example.com/m
 
 go 1.21.6
 
-require (
-	github.com/golang/mock v1.6.0
-	go.uber.org/mock v0.4.0
-)
+require github.com/golang/mock v1.6.0
